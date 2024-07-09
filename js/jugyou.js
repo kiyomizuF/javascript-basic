a913733cfbe2817926ceb253d91d89112ab00fee
kiyomizuF <shimizu.itsuki@gmail.com>
2024-07-09 14:27:34 +0900

    7/9レッスンにて、これまで勉強したJavaの振り返りとDOM操作について学習

---
+++ js/jugyou.js
@@ -0,0 +1,15 @@
+let num = 1;
+const lesson = 11;
+
+if (num <= 10) {
+  console.log("numの値は10以下です");
+}
+
+while (num <= 10) {
+  console.log(num);
+  num++;
+}
+
+for (i = 0; i <= 10; i++) {
+  console.log(i);
+}
